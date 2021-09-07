d9a1da7e53b59298be7549739e0d22667ecc57bc
Josue Rojas <josuerojasvega@gmail.com>
2021-09-06 21:39:52 -0600

    Reading from file

---
+++ maze-threads-forks/src/maze-loader/loader.c
@@ -0,0 +1,39 @@
+#ifndef FILE
+    #include<stdio.h>
+#endif
+
+#ifndef free
+    #include<stdlib.h>
+#endif
+
+typedef struct maze {
+    int width;
+    int height;
+    char **map;
+}* Maze;
+
+#define ROW_FINISH_MARK '/n'
+
+Maze load_maze(char *filename)
+{
+    Maze maze = NULL;
+
+    FILE *file = fopen(filename, "r");
+    if (file) {
+        int c;
+        
+        while((c = getc(file)) != EOF)
+            putchar(c);
+
+        fclose(file);
+    }
+
+    return maze;
+}
+
+int main() {
+
+    load_maze("xd.txt");    
+
+    return 0;
+}
