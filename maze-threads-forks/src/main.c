4c168e79c2c6e0ad92774ddd914848f530642f15
Esteban Madrigal <ememadrigal.em@gmail.com>
2021-09-08 01:04:49 -0600

    Hilos desordenado

--- maze-threads-forks/src/main.c
+++ maze-threads-forks/src/main.c
@@ -11,17 +11,27 @@ Maze original_maze = NULL;
 int main() {
     srand(time(NULL));
     original_maze = load_maze("../utils/lab1.txt");
-    original_maze->map[0][0] = 1;
-    original_maze->map[1][0] = 1;
-    original_maze->map[2][0] = 1;
-    original_maze->map[2][1] = 2;
-    original_maze->map[2][2] = 2;
-    original_maze->map[1][2] = 3;
-    original_maze->map[0][2] = 3;
-    original_maze->map[0][3] = 4;
-    original_maze->map[0][4] = 4;
-    print_maze(original_maze);
-    // create_thread('s',0,0,0);
+    create_thread('s',0,0,0,0,0);
     return 0;
 }
 
+void manual_test(){
+    original_maze->map[0][0] = 1;
+    print_maze(original_maze);
+    original_maze->map[1][0] = 1;
+    print_maze(original_maze);
+    original_maze->map[2][0] = 1;
+    print_maze(original_maze);
+    original_maze->map[2][1] = 2;
+    print_maze(original_maze);
+    original_maze->map[2][2] = 2;
+    print_maze(original_maze);
+    original_maze->map[1][2] = 3;
+    print_maze(original_maze);
+    original_maze->map[0][2] = 3;
+    print_maze(original_maze);
+    original_maze->map[0][3] = 4;
+    print_maze(original_maze);
+    original_maze->map[0][4] = 4;
+    print_maze(original_maze); 
+}
--- maze-threads-forks/src/maze-solver/solver.c
+++ maze-threads-forks/src/maze-solver/solver.c
@@ -10,90 +10,150 @@
 typedef struct Thread {
    pthread_t id; 
    char  direction;
-   int   col_start;
-   int   row_start;
+   int   start_col;
+   int   start_row;
+   int   current_col;
+   int   current_row;
    int   steps;
    char  color;
-}* Thread;
+} Thread;
 
 char* colores[6]={"\x1B[36m", "\x1B[31m", "\x1B[32m", "\x1B[33m", "\x1B[34m", "\x1B[35m"};
 #define RESET "\x1B[0m"
 
-int total_rows = 10;
-int total_columns = 10;
+/* HEADERS */
+void *walk( void *thread );
+void create_thread( char, int, int , int, int , int );
+void print_maze();
 
-// void create_thread(char, int, int, int);
+int should_die(char direction, int row, int column){
+    int toReturn = 0;
+    if( direction == 'w' && (row <= 0 || original_maze->map[row-1][column] != ' ') ){
+        toReturn = 1;
+    }
+    else if( direction == 'a' && (column <= 0 || original_maze->map[row][column-1] != ' ') ){
+        toReturn = 1;
+    } 
+    else if(direction == 's' && 
+            ( row >= original_maze->height-1 || original_maze->map[row+1][column] != ' ') ){
+        toReturn = 1;
+    } 
+    else if(direction == 'd' && 
+            ( column >= original_maze->width-1 || original_maze->map[row][column+1] != ' ') ){
+        toReturn = 1;
+    }
+    return toReturn;
+}
 
-void *walk( void *thread ){
-    struct Thread *t = (struct Thread*)thread;
-    for (int row = (*t).row_start; row < total_rows; row++ ) {
-       for (int column = (*t).col_start; column < total_columns; column++ ) {
-           //FALTA VALIDAR BORDES
-           if(original_maze->map[row-1][column] == '/' || original_maze->map[row][column-1] == '/' || original_maze->map[row+1][column] == '/' || original_maze->map[row][column+1] == '/'){
-               //SALIO
-           }else{ 
-                if (original_maze->map[row-1][column] == ' ' && (*t).direction != 'w'){
-                   create_thread('w',row,column,(*t).steps);
-                }
-                if (original_maze->map[row][column-1] == ' ' && (*t).direction != 'a'){
-                    create_thread('a',row,column,(*t).steps);
-                }
-                if (original_maze->map[row+1][column] == ' ' && (*t).direction != 's'){
-                    create_thread('s',row,column,(*t).steps);
-                }
-                if (original_maze->map[row][column+1] == ' ' && (*t).direction != 'd'){
-                    create_thread('d',row,column,(*t).steps);
-                } 
-
-                if((*t).direction == 'w' && original_maze->map[row-1][column] != ' '){
-                    pthread_join( (*t).id, NULL); //Muere el hilo
-                }
-                else if((*t).direction == 'a' && original_maze->map[row][column-1] != ' '){
-                    pthread_join( (*t).id, NULL); //Muere el hilo
-                } 
-                else if((*t).direction == 's' && original_maze->map[row+1][column] != ' '){
-                    pthread_join( (*t).id, NULL); //Muere el hilo
-                } 
-                else if((*t).direction == 'd' && original_maze->map[row][column+1] != ' '){
-                    pthread_join( (*t).id, NULL); //Muere el hilo
-                }else{
-                    (*t).steps++;
-
-                    if((*t).direction == 'w'){
-                        original_maze->map[row-1][column] = (char) (*t).color;
-                    }
-                    else if((*t).direction == 'a'){
-                        original_maze->map[row][column-1] = (char) (*t).color;
-                    } 
-                    else if((*t).direction == 's'){
-                        original_maze->map[row+1][column] = (char) (*t).color;
-                    } 
-                    else if((*t).direction == 'd'){
-                        original_maze->map[row][column+1] = (char) (*t).color;
-                    }
-                }
-           }
-       }
+void paint_path(int color, int row, int column){
+    if(row < original_maze->height && column < original_maze->width && row >= 0 && column >= 0){
+        original_maze->map[row][column] = (char) color;
+        print_maze();
     }
 }
-void create_thread(char direction, int row, int column, int steps){
-    pthread_t thread1;
-    int  iret1;
-    Thread first_thread;
-    first_thread->id = thread1;
-    first_thread->direction = direction;
-    first_thread->row_start = row;
-    first_thread->col_start = column;
-    first_thread->steps = steps;
-    first_thread->color = rand()%6;
 
-    iret1 = pthread_create( &thread1, NULL, walk, (void*) first_thread);
-    // pthread_join( (*t).id, NULL);
+void take_a_step(Thread *thread){
+    // printf("Take a step\n");
+    thread->steps++;
+    if(thread->direction == 'w'){
+        thread->current_row--;
+    }
+    else if(thread->direction == 'a'){
+        thread->current_col--;
+    } 
+    else if(thread->direction == 's'){
+        thread->current_row++;
+    } 
+    else if(thread->direction == 'd'){
+        thread->current_col++;
+    }
+    
 }
 
-void print_maze(Maze original_maze){
-	for (int row = 0; row < total_rows; row++ ) {
-       for (int column = 0; column < total_columns; column++ ) {
+int is_at_finish(int row, int column){
+    if( (row > 0 && row < original_maze->height-1  && original_maze->map[row-1][column] == '/') 
+        || (column > 0 && row < original_maze->width-1 && original_maze->map[row][column-1] == '/') 
+        || (row < original_maze->height-1 && row > 0 && original_maze->map[row+1][column] == '/') 
+        || (column < original_maze->width-1 && column > 0 && original_maze->map[row][column+1] == '/') ){
+        return 1;
+    }else{
+        return 0;
+    }
+    return 0;
+}
+
+void *walk( void *thread ){
+    
+    int total_rows = original_maze->height;
+    int total_columns = original_maze->width;
+    
+    Thread *current_thread = (Thread*) thread;
+
+    int row = current_thread->current_row;
+    int column = current_thread->current_col;
+
+    int is_death = 0;
+
+    while(!is_death){
+        paint_path(current_thread->color, row, column);
+        if(is_at_finish(row, column)){
+            printf("Congrats");
+            // break;
+        } 
+        //CREAR NUEVOS CAMINOS
+        if (row != 0 && original_maze->map[row-1][column] == ' ' && (current_thread->direction != 'w')){
+            //printf("%c CREAR CAMINO ARRIBA\n",current_thread->direction);
+            create_thread('w', row, column, row-1, column, current_thread->steps);
+        }
+        if (column != 0 && original_maze->map[row][column-1] == ' ' && (current_thread->direction != 'a')){
+            //printf("%c CREAR CAMINO IZQUIERDA\n",current_thread->direction);
+            create_thread('a', row, column, row, column-1, current_thread->steps);
+        }
+        if (row != total_rows-1 && original_maze->map[row+1][column] == ' ' && (current_thread->direction != 's')){
+            //printf("CREAR CAMINO ABAJO\n");
+            create_thread('s', row, column, row+1, column, current_thread->steps);
+        }
+        if (column != total_columns-1, original_maze->map[row][column+1] == ' ' && (current_thread->direction != 'd')){
+            //printf("CREAR CAMINO DERECHA\n");
+            create_thread('d', row, column, row, column+1, current_thread->steps);
+        } 
+        
+        is_death = should_die( current_thread->direction, row, column);
+        //CAMINA EL HILO ACTUAL
+        if(!is_death){
+            take_a_step(current_thread);
+            row = current_thread->current_row;
+            column = current_thread->current_col;
+        }
+        // printf("%d\n",current_thread->color);
+    }
+    // printf("%d %d\n",current_thread->current_row,current_thread->current_col);
+}
+
+void create_thread(char direction, int start_row, int start_col, int current_row, int current_col, int steps){
+    pthread_t thread1;
+    int  iret1;
+
+    Thread first_thread;
+
+    first_thread.id = thread1;
+    first_thread.direction = direction;
+    first_thread.start_row = start_row;
+    first_thread.start_col = start_col;
+    first_thread.current_row = current_row;
+    first_thread.current_col = current_col;
+    first_thread.steps = steps;
+    first_thread.color = rand()%6;
+
+    iret1 = pthread_create( &thread1, NULL, walk, (void*) &first_thread);
+    pthread_join( thread1, NULL);
+}
+
+void print_maze(){
+    sleep(1);
+    printf("\e[1;1H\e[2J");   
+	for (int row = 0; row < original_maze->height; row++ ) {
+       for (int column = 0; column < original_maze->width; column++ ) {
            if(original_maze->map[row][column] == '*'){
                 printf("\u2592");    
            }else if (original_maze->map[row][column] == ' ' || original_maze->map[row][column] == '/'){
